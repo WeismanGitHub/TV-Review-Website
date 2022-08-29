acd674cbd5fcc26161f05c444160120c6f02eed5
Weisman <weisman123@outlook.com>
2022-08-29 03:17:30 -0700

    created authentication controllers file

---
+++ server/controllers/authentication-controllers.js
@@ -0,0 +1,39 @@
+const UserSchema = require('../schemas/user-schema')
+
+const register = async (req, res) => {
+    const user = await UserSchema.create(req.body)
+    const token = user.createJWT()
+
+    res.status(201)
+    .cookie('token', token)
+    .redirect('/')
+}
+
+const login = async (req, res) => {
+    const { name, password } = req.body
+    const user = await UserSchema.findOne({ name: name })
+
+    if (!user) {
+        throw new Error('Please provide a valid name.')
+    }
+    
+    const PasswordIsCorrect = await user.checkPassword(password)
+    
+    if (!PasswordIsCorrect) {
+        throw new Error('Please provide the correct password.')
+    }
+
+    const token = user.createJWT()
+
+    res.status(200)
+    .cookie('token', token)
+    .redirect('/')
+}
+
+const logout = async (req, res) => {
+    res.status(200)
+    .clearCookie('token')
+    .redirect('/authentication')
+}
+
+module.exports = { login, register, logout }
