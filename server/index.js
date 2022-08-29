23a1c9f60a7f168aeae29e20dcabbf30cf9e1baf
Weisman <weisman123@outlook.com>
2022-08-29 14:49:45 -0700

    server now runs

--- server/index.js
+++ server/index.js
@@ -10,10 +10,10 @@ const cors = require('cors');
 require('express-async-errors');
 require("dotenv").config();
 
-const authenticationMiddleware = require('./middleware/authentication-middleware')
-const authenticationRouter = require('./routers/authentication-router')
-const errorHandler = require('./middleware/error-handler')
-const userRouter = require('./routers/user-router')
+// const authenticationMiddleware = require('./middleware/authentication-middleware')
+// const authenticationRouter = require('./routers/authentication-router')
+// const errorHandler = require('./middleware/error-handler')
+// const userRouter = require('./routers/user-router')
 
 const app = express();
 const limiter = rateLimit({
@@ -24,7 +24,7 @@ const limiter = rateLimit({
 })
 
 app.use(limiter)
-app.use(express.static(path.join(__dirname, '../', 'client', 'build')))
+app.use(express.static(path.resolve(__dirname, '../build')))
 app.use(helmet.contentSecurityPolicy({ directives: { 'script-src-attr': null } }));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
@@ -32,20 +32,20 @@ app.use(cookieParser());
 app.use(compression());
 app.use(cors())
 
-app.use('/api/v1/user', authenticationMiddleware, userRouter)
-app.use('/api/v1/authentication', authenticationRouter)
+// app.use('/api/v1/user', authenticationMiddleware, userRouter)
+// app.use('/api/v1/authentication', authenticationRouter)
 
 app.get('*', (req, res) => {
     res.sendFile('index.html', { root: path.join(__dirname, '../client/build/') });
 });
 
-app.use(errorHandler)
+// app.use(errorHandler)
 
 try {
     const port = process.env.PORT || 5000
     mongoose.connect(process.env.MONGO_URI, { autoIndex: true })
     console.log('Connected to database...')
-    server.listen(port, console.log(`listening on port ${port}...`));
+    app.listen(port, console.log(`listening on port ${port}...`));
 } catch (err) {
     console.error(err);
 };
