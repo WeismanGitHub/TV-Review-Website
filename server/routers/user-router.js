6ebd00c65f9f7f129f8df76714dedf585ad7c03f
Weisman <weisman123@outlook.com>
2022-08-31 13:07:54 -0700

    user router made

---
+++ server/routers/user-router.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const {
+    updateUser,
+    deleteUser,
+    getUser,
+} = require('../controllers/user-controllers')
+
+const router = express.Router()
+
+router.route('/update').post(updateUser)
+router.route('/delete').post(deleteUser)
+router.route('/:userId?').get(getUser)
+
+module.exports = router
