42e321a71970298852670a1f51a37f3f67565a3d
Weisman <weisman123@outlook.com>
2022-08-31 13:06:50 -0700

    user controllers made

---
+++ server/controllers/user-controllers.js
@@ -0,0 +1,37 @@
+const UserSchema = require('../schemas/user-schema')
+
+const updateUser = async (req, res) => {
+    const user = await UserSchema.findByIdAndUpdate(
+        req.userId,
+        req.body
+    ).select('_id')
+
+    const token = user.createJWT()
+
+    res.status(200)
+    .cookie('token', token)
+    .end()
+}
+
+const deleteUser = async (req, res) => {
+    await UserSchema.deleteOne({ _id: req.userId })
+
+    res.status(200)
+    .clearCookie('token')
+    .redirect('/authentication')
+}
+
+const getUser = async (req, res) => {
+    const user = await UserSchema.findById(req.params.userId || req.userId).select('-_id -password').lean()
+
+    if (!user) throw new Error('User does not exist.')
+
+    res.status(StatusCodes.OK)
+    .send(user)
+}
+
+module.exports = {
+    updateUser,
+    deleteUser,
+    getUser,
+}
