192ab2427875dd3262366816a417fba8c8aa689f
Weisman <weisman123@outlook.com>
2022-09-01 14:57:30 -0700

    split getUser route into getUser and getSelf routes

--- server/routers/user-router.js
+++ server/routers/user-router.js
@@ -5,12 +5,14 @@ const {
     updateUser,
     deleteUser,
     getUser,
+    getSelf
 } = require('../controllers/user-controllers')
 
 const router = express.Router()
 
 router.route('/update').post(authenticationMiddleware, updateUser)
 router.route('/delete').post(authenticationMiddleware, deleteUser)
-router.route('/:userId?').get(getUser)
+router.route('/').get(authenticationMiddleware, getSelf)
+router.route('/:userId').get(getUser)
 
 module.exports = router
