71da2dbeadef98a5bc5dca0a3059373bce8f2580
Weisman <weisman123@outlook.com>
2022-09-04 22:02:18 -0700

    created boilerplate for review controllers

---
+++ server/controllers/review-controllers.js
@@ -0,0 +1,20 @@
+const ReviewSchema = require('../schemas/review-schema')
+
+const createReview = async (req, res) => {
+}
+
+const deleteReview= async (req, res) => {
+}
+
+const getReview = async (req, res) => {
+}
+
+const updateReview = async (req, res) => {
+}
+
+module.exports = {
+    updateReview,
+    createReview,
+    deleteReview,
+    getReview,
+}
