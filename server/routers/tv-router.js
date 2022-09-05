fd4d5739d5c0cdf44071465e607d642ce669640c
Weisman <weisman123@outlook.com>
2022-09-05 05:14:12 -0700

    added searchTV router

--- server/routers/tv-router.js
+++ server/routers/tv-router.js
@@ -1,8 +1,9 @@
-const { getTVList } = require('../controllers/tv-controllers')
+const { getTV, searchTV } = require('../controllers/tv-controllers')
 const express = require('express')
 
 const router = express.Router()
 
-router.route('/').post(getTVList)
+router.route('/:name').get(getTV)
+router.route('/search/:phrase').get(searchTV)
 
 module.exports = router
