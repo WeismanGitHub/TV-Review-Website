b2f9ed9a65e9a21e963e0c883755282c196a76c4
Weisman <weisman123@outlook.com>
2022-09-06 02:44:55 -0700

    put res on one line

--- server/controllers/tv-controllers.js
+++ server/controllers/tv-controllers.js
@@ -12,22 +12,19 @@ const searchTV = async (req, res) => {
         media_type: result.media_type
     } })
 
-    res.status(200)
-    .json(results)
+    res.status(200).json(results)
 }
 
 const getShow = async (req, res) => {
     const result = (await axios.get(`https://api.themoviedb.org/3/tv/${req.params.id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`)).data
 
-    res.status(200)
-    .json(result)
+    res.status(200).json(result)
 }
 
 const getMovie = async (req, res) => {
     const result = (await axios.get(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`)).data
 
-    res.status(200)
-    .json(result)
+    res.status(200).json(result)
 }
 
 module.exports = {
