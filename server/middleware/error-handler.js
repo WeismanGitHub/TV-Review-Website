9e36c5c5a2c18f30804edf058d332257bde2cb51
Weisman <weisman123@outlook.com>
2022-09-06 02:41:46 -0700

    added error handling for 404 error from tmdb

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,7 +1,12 @@
 const errorHandler = (err, req, res, next) => {
     console.log(err)
 
-    res.status( err.statusCode || 500).send(String(err.message))
+    if (err.message == 'Request failed with status code 404') {
+        return res.status(404).send('Not found!')
+    }
+
+
+    res.status(err.statusCode || 500).send(String(err.message))
 }
 
 module.exports = errorHandler
