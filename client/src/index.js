ff97e688e477d88ee979cad686126850a5135b1d
Weisman <weisman123@outlook.com>
2022-09-07 14:11:12 -0700

    added tv route

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css';
 
 import Authentication from './authentication'
+import TVPage from './tv-page'
 import Self from './self'
 import Home from './home'
 
@@ -16,6 +17,7 @@ root.render(
                 <Route exact path='/' element={<Home/>}/>
                 <Route exact path='/account' element={<Self/>}/>
                 <Route exact path='/authentication' element={Authentication}/>
+                <Route exact path='/:id' element={<TVPage/>}/>
                 <Route path='*' element={<h1>This page doesn't exist!</h1>}/>
             </Routes>
         </Router>
