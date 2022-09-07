1c7383639d28cddc81c1ed66617153876e7ee251
Weisman <weisman123@outlook.com>
2022-09-07 13:24:01 -0700

    set up boilerplate for tv page

---
+++ client/src/tv-page.js
@@ -0,0 +1,12 @@
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+
+const axios = require('axios').default;
+
+function TVPage() {
+   
+}
+
+export default TVPage
