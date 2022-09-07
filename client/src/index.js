9bab472d7e87ace7f574352049735ca4796bbea3
Weisman <weisman123@outlook.com>
2022-09-07 16:19:17 -0700

    fixed

--- client/src/index.js
+++ client/src/index.js
@@ -17,7 +17,7 @@ root.render(
                 <Route exact path='/' element={<Home/>}/>
                 <Route exact path='/account' element={<Self/>}/>
                 <Route exact path='/authentication' element={Authentication}/>
-                <Route exact path='/:id' element={<TVPage/>}/>
+                <Route exact path='/:tvType/:id' element={<TVPage/>}/>
                 <Route path='*' element={<h1>This page doesn't exist!</h1>}/>
             </Routes>
         </Router>
