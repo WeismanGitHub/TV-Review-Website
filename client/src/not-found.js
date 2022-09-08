6f463102695b8e41015142e310f674bf5559d5d6
Weisman <weisman123@outlook.com>
2022-09-07 17:27:27 -0700

    created not found

---
+++ client/src/not-found.js
@@ -0,0 +1,12 @@
+import NavBar from './navbar'
+
+function NotFound() {
+    return (<>
+        <NavBar/>
+        <br/>
+        <br/>
+        <h1>This page doesn't exist!</h1>
+    </>)
+}
+
+export default Home;
