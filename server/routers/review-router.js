262672889943ebe954857b24c4d5f6d3d5132f17
Weisman <weisman123@outlook.com>
2022-09-11 11:43:40 -0700

    added reviewMiddleware

--- server/routers/review-router.js
+++ server/routers/review-router.js
@@ -1,4 +1,5 @@
 const authenticationMiddleware = require('../middleware/authentication-middleware')
+const reviewsMiddleware = require('../middleware/reviews-middleware.js')
 const express = require('express')
 
 const {
@@ -13,6 +14,6 @@ const router = express.Router()
 router.route('/').post(authenticationMiddleware, createReview)
 router.route('/update/:id').post(authenticationMiddleware, updateReview)
 router.route('/delete/:id').post(authenticationMiddleware, deleteReview)
-router.route('/:type/:id').get(getReviews)
+router.route('/:type/:id').get(reviewsMiddleware, getReviews)
 
 module.exports = router
