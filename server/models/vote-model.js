0ebb72228f165ef7cd95184dffa41e15cbc7b72a
Weisman <weisman123@outlook.com>
2022-09-12 11:10:39 -0700

    added userId

--- server/models/vote-model.js
+++ server/models/vote-model.js
@@ -9,6 +9,10 @@ const VoteSchema = new mongoose.Schema({
     reviewId: {
         type: mongoose.Types.ObjectId,
         required: [true, 'Please provide the review id.']
+    },
+    userId: {
+        type: mongoose.Types.ObjectId,
+        required: [true, 'Please provide a user id.']
     }
 })
 
