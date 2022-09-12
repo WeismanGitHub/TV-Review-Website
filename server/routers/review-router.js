499b11dcb68259f9cec0a7d4426d0b83f8697dc0
Weisman <weisman123@outlook.com>
2022-09-12 12:04:31 -0700

    added vote route

--- server/routers/review-router.js
+++ server/routers/review-router.js
@@ -6,12 +6,14 @@ const {
     updateReview,
     createReview,
     deleteReview,
-    getReviews
+    getReviews,
+    vote
 } = require('../controllers/review-controllers')
 
 const router = express.Router()
 
 router.route('/').post(authenticationMiddleware, createReview)
+router.route('/vote/:reviewId').post(authenticationMiddleware, vote)
 router.route('/update/:id').post(authenticationMiddleware, updateReview)
 router.route('/delete/:id').post(authenticationMiddleware, deleteReview)
 router.route('/:type/:id').get(reviewsMiddleware, getReviews)
