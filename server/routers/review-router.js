d0b16e072c4c47937f8ad5b92845bb00c8d26b66
Weisman <weisman123@outlook.com>
2022-09-15 02:38:48 -0700

    created new route for deleting reviews

--- server/routers/review-router.js
+++ server/routers/review-router.js
@@ -16,10 +16,13 @@ const router = express.Router()
 router.route('/')
 .post(authenticationMiddleware, createReview)
 .patch(authenticationMiddleware, updateReview)
-.delete(authenticationMiddleware, deleteReview)
+
+// The delete method doesn't detect request bodies.
+router.route('/delete').post(authenticationMiddleware, deleteReview)
 
 router.route('/vote').post(authenticationMiddleware, vote)
 .delete(authenticationMiddleware, deleteVote)
+
 router.route('/:type/:id').get(reviewsMiddleware, getReviews)
 
 module.exports = router
