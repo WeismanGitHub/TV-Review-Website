7283422e566c1afaa5b2572b1684444f8f7f66df
Weisman <weisman123@outlook.com>
2022-09-16 01:27:01 -0700

    created validation error

---
+++ server/errors/validation-error.js
@@ -0,0 +1,8 @@
+class ValidationError extends Error {
+    constructor(message) {
+        super(message || 'Validation Error')
+        this.statusCode = 400
+    }
+}
+
+module.exports = ValidationErro
