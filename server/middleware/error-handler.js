7fd32f548a42a47b3bb9ea7f35fdb67f2534dfc9
Weisman <weisman123@outlook.com>
2022-09-15 21:27:10 -0700

    added error handling for getting tmdb results

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,10 +1,15 @@
 const errorHandler = (err, req, res, next) => {
-    console.log(err)
-
     if (err.message == 'Request failed with status code 404') {
+        console.log(err)
         return res.status(404).send('Not found!')
     }
-
+    
+    if (err.code == 'ENOTFOUND') {
+        console.log(err.message)
+        return res.status(500).send('Error getting results.')
+    }
+    
+    console.log(err)
     res.status(err.statusCode || 500).send(String(err.message))
 }
 
