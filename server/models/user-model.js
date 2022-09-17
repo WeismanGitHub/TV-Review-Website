5003f76e7d1764584b348eca5cb99b9d688196e5
Weisman <weisman123@outlook.com>
2022-09-16 17:06:58 -0700

    removed updatedAt

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -30,7 +30,7 @@ const UserSchema = new mongoose.Schema({
         default: 'user',
         enum: ['user', 'moderator', 'administrator']
     }
-}, { timestamps: true })
+}, { timestamps: { createdAt: true, updatedAt: false } })
 
 UserSchema.pre('save', async function() {
     const salt = await bcrypt.genSalt(10)
