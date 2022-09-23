db95d366353fc98f087e02d3c17c471f3fd55796
Weisman <weisman123@outlook.com>
2022-09-22 21:30:16 -0700

    added trending to homepage

--- client/src/home.js
+++ client/src/home.js
@@ -1,9 +1,12 @@
-import NavBar from './navbar'
+import Trending from './home/trending'
 import Search from './home/search'
+import NavBar from './navbar'
 
 function Home() {
     return (<>
         <NavBar/>
+        <br/>
+        <Trending/>
         <br/>
         <Search/>
     </>)
