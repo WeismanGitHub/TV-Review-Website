0024d9e8e67a61688edd6a829e1cd2a5f62f87b6
Weisman <weisman123@outlook.com>
2022-09-23 23:36:26 -0700

    created trending

---
+++ client/src/home/trending.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+const axios = require('axios').default;
+
+function Trending() {
+    const [trending, setTrending] = useState([{ title: 'loading...'}])
+
+    useEffect(() => {
+        axios.get(`api/tv/trending`)
+        .then(res => setTrending(res.data))
+        .catch(err => toast.error(err.response.data))
+    }, []);
+    
+    console.log(trending[0])
+
+    return (<>
+        <div class='trendingTVList'>
+            {trending.map(tv => <a class='trendingTV' href={`/${tv.media_type == 'movie' ? 'movie' : 'show'}/${tv.id}`}>{tv.title}</a>)}
+        </div>
+    </>)
+}
+
+export default Trending;
