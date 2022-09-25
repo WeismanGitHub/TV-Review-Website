40bf135932f119265fca8638b520ba6dc84dee8c
Weisman <weisman123@outlook.com>
2022-09-25 15:24:37 -0700

    created homepage

---
+++ client/src/mod/home.js
@@ -0,0 +1,12 @@
+import Reports from './home/reports'
+import NavBar from '../navbar'
+
+function Home() {
+    return (<>
+        <NavBar/>
+        <br/>
+        <Reports/>
+    </>)
+}
+
+export default Home;
