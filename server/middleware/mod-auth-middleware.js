de78f1fa358018786d093d0011f342ed30179072
Weisman <weisman123@outlook.com>
2022-09-27 14:21:23 -0700

    only checks req user level

--- server/middleware/mod-auth-middleware.js
+++ server/middleware/mod-auth-middleware.js
@@ -1,30 +1,9 @@
-const UserModel = require('../models/user-model')
-const jwt = require('jsonwebtoken')
-
 const modAuthMiddleware = async (req, res, next) => {
-    const token = req.cookies.token
-
-    if (!token) {
-        throw new Error('Please log in!')
+    if (req.user.level == 'user') {
+        return res.status(401).redirect('/authentication')
     }
-    
-    try {
-        const payload = jwt.verify(token, process.env.JWT_SECRET)
 
-        const user = await UserModel.findById(req.userId).select('-_id level').lean()
-
-        if (user.level == 'user') {
-            throw new Error('You are not a mod.')
-        }
-
-        req.user = { _id: payload._id, level: user.level }
-
-        next()
-    } catch (error) {
-        res.status(401)
-        .clearCookie('token')
-        .redirect('/authentication')
-    }
+    next()
 }
 
 module.exports = modAuthMiddleware
