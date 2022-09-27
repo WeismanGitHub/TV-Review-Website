7e40c5ffd755f08b92eed5f12d7fffe73e109ded
Weisman <weisman123@outlook.com>
2022-09-27 14:36:46 -0700

    changed req.user._id to req.user.id

--- server/middleware/authentication-middleware.js
+++ server/middleware/authentication-middleware.js
@@ -9,7 +9,7 @@ const authentication = async (req, res, next) => {
     
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = { _id: payload._id, level: payload.level }
+        req.user = { id: payload._id, level: payload.level }
         next()
     } catch (error) {
         res.status(401)
