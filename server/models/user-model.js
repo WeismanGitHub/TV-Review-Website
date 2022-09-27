921882d8cd09f0cf8821a4351fab2cbbbcb362c2
Weisman <weisman123@outlook.com>
2022-09-27 00:13:05 -0700

    added level to createJWT

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -46,7 +46,7 @@ UserSchema.pre('save', async function() {
 
 UserSchema.methods.createJWT = function() {
     return jwt.sign(
-        { _id: this._id },
+        { _id: this._id, level: this.level },
         process.env.JWT_SECRET,
         { expiresIn: process.env.JWT_LIFETIME },
     )
