cc7da6df77bcd929caa677f795c17a8cda1a5b26
Weisman <weisman123@outlook.com>
2022-09-30 19:29:53 -0700

    added mod routes

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import NotFound from './not-found'
 import TVPage from './tv-page'
 import Self from './self'
 import Home from './home'
-import Mod from './mod'
+import ModRoutes from './mod'
 
 const root = createRoot(document.getElementById('root'));
 
@@ -18,7 +18,7 @@ root.render(
     <>
         <Router>
             <Routes>
-                <Mod/>
+                {ModRoutes}
                 <Route exact path='/' element={<Home/>}/>
                 <Route exact path='/account' element={<Self/>}/>
                 <Route exact path='/authentication' element={Authentication}/>
