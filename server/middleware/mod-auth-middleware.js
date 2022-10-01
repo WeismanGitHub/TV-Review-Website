b20fedeb3d826ddebe46ab0f0211555d1de4bba5
Weisman <weisman123@outlook.com>
2022-10-01 16:17:46 -0700

    sends error message doesnt redirect

--- server/middleware/mod-auth-middleware.js
+++ server/middleware/mod-auth-middleware.js
@@ -1,6 +1,6 @@
 const modAuthMiddleware = async (req, res, next) => {
     if (req.user.level == 'user') {
-        return res.status(401).redirect('/authentication')
+        return res.status(401).send('Must be a mod.')
     }
 
     next()
