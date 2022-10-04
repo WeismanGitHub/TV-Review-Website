5d6ca5650150b1e062233853ccbae7d4da775f24
Weisman <weisman123@outlook.com>
2022-10-03 18:00:43 -0700

    clear level cookie

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -4,12 +4,14 @@ const cookie = new Cookie();
 
 function NavBar() {
     const token = cookie.get('token')
+    const level = cookie.get('level')
 
     return (
         <div>
             <div class='navbar'>
                 <a href='/' class="customButton">Home</a>
                 {token ? <a href='/account' class="customButton">Account</a> : <a href='/authentication' class='customButton'>Login/Register</a>}
+                {(level == 'administrator' || 'moderator') && <a class='customButton' href='/mod'>Mod</a>}
                 {token && <Logout class='customButton'/>}
             </div>
             <br/>
--- server/controllers/authentication-controllers.js
+++ server/controllers/authentication-controllers.js
@@ -45,7 +45,7 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
     res.status(200)
     .clearCookie('token')
-    .cookie('level')
+    .clearCookie('level')
     .redirect('/authentication')
 }
 
