82d8a06fa65011ef34d99003586b681d5c788e2d
Weisman <weisman123@outlook.com>
2022-10-05 20:23:31 -0700

    fixed default page number

--- server/controllers/mod-controller.js
+++ server/controllers/mod-controller.js
@@ -9,7 +9,7 @@ const deleteReview = async (req, res) => {
 
 const getReports = async (req, res) => {
     const reports = await ReportModel.find({ resolved: req.query.status || false })
-    .skip((req.query.page || 1) * 10).limit(10).select('-__v').lean()
+    .skip((req.query.page || 0) * 10).limit(10).select('-__v').lean()
     console.log(reports)
     res.status(200).json(reports)
 }
