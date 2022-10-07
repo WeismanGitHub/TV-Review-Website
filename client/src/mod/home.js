862abe986afb64a8b989d83a2f1057ad0e48a0e5
Weisman <weisman123@outlook.com>
2022-10-07 02:12:38 -0700

    i forgot what i added sorry

--- client/src/mod/home.js
+++ client/src/mod/home.js
@@ -1,7 +1,17 @@
-import Reports from './home/reports'
+import { useNavigate } from 'react-router-dom';
+import Cookie from 'universal-cookie';
+import Reports from './reports'
 import NavBar from '../navbar'
 
+const cookie = new Cookie();
+
 function Home() {
+    const navigate = useNavigate();
+
+    if (!cookie.get('token')) {
+        navigate('/authentication');
+    }
+
     return (<>
         <NavBar/>
         <br/>
