9c431cea323bef23c3a81529abee71711e4d81cf
Weisman <weisman123@outlook.com>
2022-10-25 03:26:46 -0700

    removed delete vote route

--- server/routers/review-router.js
+++ server/routers/review-router.js
@@ -8,14 +8,13 @@ const {
     deleteReview,
     reportReview,
     getReviews,
-    deleteVote,
     vote
 } = require('../controllers/review-controllers')
 
 const router = Router()
 
 router.route('/').post(authenticationMiddleware, createReview).patch(authenticationMiddleware, updateReview)
-router.route('/vote').post(authenticationMiddleware, vote).delete(authenticationMiddleware, deleteVote)
+router.route('/vote').post(authenticationMiddleware, vote)
 router.route('/report').post(authenticationMiddleware, reportReview)
 router.route('/delete').post(authenticationMiddleware, deleteReview)
 router.route('/:type/:id').get(reviewsAuthMiddleware, getReviews)
