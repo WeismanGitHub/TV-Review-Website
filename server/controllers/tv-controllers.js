c309724df441da20ad30ba9a1f55bafbe4c43544
Weisman <weisman123@outlook.com>
2022-10-28 18:10:58 -0700

    improved var name and formatting

--- server/controllers/tv-controllers.js
+++ server/controllers/tv-controllers.js
@@ -2,15 +2,16 @@ const axios = require('axios');
 require("dotenv").config()
 
 const searchTV = async (req, res) => {
-    const results = (await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=${req.query.page || 1}&include_adult=true&query=${req.query.phrase}`)).data.results
-    .filter(
-        result => result.media_type == 'tv' || result.media_type == 'movie'
-    ).map(
-        result => { return {
-        title: result.original_name || result.original_title || result.name,
-        id: result.id,
-        media_type: result.media_type
-    } })
+    let results = (await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=${req.query.page || 1}&include_adult=true&query=${req.query.phrase}`)).data.results
+    
+    results = results.filter(result => result.media_type == 'tv' || result.media_type == 'movie')
+    .map(result => {
+        return {
+            title: result.original_name || result.original_title || result.name,
+            id: result.id,
+            mediaType: result.media_type
+        }
+    })
 
     res.status(200).json(results)
 }
