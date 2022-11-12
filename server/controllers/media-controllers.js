9f7c85b9f350f8a61c1f9dedf2987f513253a86b
Weisman <weisman123@outlook.com>
2022-11-12 06:44:36 -0800

    changed tv to media

---
+++ server/controllers/media-controllers.js
@@ -0,0 +1,73 @@
+const NodeCache = require( "node-cache");
+const axios = require('axios');
+require("dotenv").config()
+
+const trendingMediaCache = new NodeCache({ stdTTL: 86400, checkperiod: 3600 });
+
+function standardizeMedia(media) {
+    return media.map(result => {
+        return {
+            title: result.original_name || result.original_title || result.name,
+            id: result.id,
+            mediaType: result.media_type
+        }
+    })
+}
+
+const searchMedia = async (req, res) => {
+    const results = standardizeMedia((await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=${req.query.page || 1}&include_adult=true&query=${req.query.phrase}`)).data.results
+    .filter(result => result.media_type == 'tv' || result.media_type == 'movie'))
+
+    res.status(200).json(results)
+}
+
+const getMedia = async (req, res) => {
+    const { type, id } = req.params
+    
+    const result = (await axios.get(`https://api.themoviedb.org/3/${type}/${id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`)).data
+
+    let media = {
+        title: result.original_name || result.original_title || result.name,
+        genres: result.genres.map(genre => genre.name).join(', '),
+        release: type == 'movie' ? result.release_date : result.first_air_date,
+        overview: result.overview,
+    }
+
+    if (type == 'tv') {
+        media = {
+            ...media,
+            episodeCount: result.number_of_episodes,
+            seasons: result.seasons.map(season => {
+                return {
+                    episodeCount: season.episode_count,
+                    airDate: season.air_date,
+                    name: season.name,
+                }
+            }),
+        }
+    }
+
+    res.status(200).json(media)
+}
+
+const getTrendingMedia = async (req, res) => {
+    let trendingMedia = trendingMediaCache.get('trendingMedia')
+
+    if (!trendingMedia) {
+        const trendingMovies =(await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.TMDB_API_KEY}`)).data.results
+        const trendingTV = (await axios.get(`https://api.themoviedb.org/3/trending/tv/week?api_key=${process.env.TMDB_API_KEY}`))
+        .data.results
+
+        trendingMedia = { ...standardizeMedia(trendingMovies), ...standardizeMedia(trendingTV) }
+
+        trendingMediaCache.set('trendingMedia', trendingMedia)
+    }
+    
+    res.status(200).json(trendingMedia)
+}
+
+module.exports = {
+    getTrendingMedia,
+    searchMedia,
+    getMedia,
+}
