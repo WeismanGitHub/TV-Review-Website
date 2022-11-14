4a238b020aa89ceb427345f98c548a5f680501d6
Weisman <weisman123@outlook.com>
2022-11-13 21:47:12 -0800

    created

---
+++ server/tests/authentication.test.js
@@ -0,0 +1,12 @@
+const { faker } = require('@faker-js/faker');
+const request = require('supertest');
+const app = require('../app');
+  
+it('POST /register => create an account', () => {
+  return request(app)
+    .post('/api/authentication/register')
+    .send({ name: faker.name.firstName, password: faker.internet.password })
+    .expect(200)
+    
+    .then(res => { console.log(res.headers) });
+});
