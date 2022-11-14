fd558bb1e9e22f9d2f9eb88db4b964b848dee355
Weisman <weisman123@outlook.com>
2022-11-14 05:50:46 -0800

    added another name length test

--- server/tests/authentication/register.test.js
+++ server/tests/authentication/register.test.js
@@ -3,18 +3,29 @@ const request = require('supertest');
 const app = require('../../app');
   
 it('POST /register => create an account', () => {
-  return request(app)
+    return request(app)
     .post('/api/authentication/register')
-    .send(`name=${faker.name.firstName()}`)
-    .send(`password=${faker.internet.password()}`)
     .set('Accept', 'application/json')
+    .send(`name=${faker.name.fullName()}`)
+    .send(`password=password`)
     .expect(201)
 });
 
+// NAME
 it('POST /register => reject because name is too long', () => {
-  return request(app)
+    return request(app)
     .post('/api/authentication/register')
+    .set('Accept', 'application/json')
     .send(`name=123456789_12345`) // string length == 15
-    .send(`password=${faker.internet.password()}`)
+    .send(`password=password}`)
+    .expect(400)
+});
+
+it('POST /register => reject because name is too short', () => {
+    return request(app)
+    .post('/api/authentication/register')
+    .set('Accept', 'application/json')
+    .send(`name=`)
+    .send(`password=password}`)
     .expect(400)
 });
