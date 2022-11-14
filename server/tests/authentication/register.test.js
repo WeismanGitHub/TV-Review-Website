1bf2fde1185ba65407b60bf318fa2332781ef2f0
Weisman <weisman123@outlook.com>
2022-11-14 01:51:26 -0800

    added test

--- server/tests/authentication/register.test.js
+++ server/tests/authentication/register.test.js
@@ -10,3 +10,11 @@ it('POST /register => create an account', () => {
     .set('Accept', 'application/json')
     .expect(201)
 });
+
+it('POST /register => reject because name is too long', () => {
+  return request(app)
+    .post('/api/authentication/register')
+    .send(`name=123456789_12345`) // string length == 15
+    .send(`password=${faker.internet.password()}`)
+    .expect(400)
+});
