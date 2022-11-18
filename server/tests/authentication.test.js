c19e1b5a88852c273a13676c77dbadbe4f7d918a
Weisman <weisman123@outlook.com>
2022-11-18 04:39:15 -0800

    completely restructured tests

--- server/tests/authentication.test.js
+++ server/tests/authentication.test.js
@@ -1,12 +1,61 @@
 const { faker } = require('@faker-js/faker');
 const request = require('supertest');
 const app = require('../app');
-  
-it('POST /register => create an account', () => {
-  return request(app)
-    .post('/api/authentication/register')
-    .send({ name: faker.name.firstName, password: faker.internet.password })
-    .expect(200)
+
+describe('authentication', () => {
+    describe('POST /api/register',  () => {
+        it('returns 201 because req.body is valid', () => { // TODO: Deconstruct this, delete account
+            return request(app)
+            .post('/api/authentication/register')
+            .set('Accept', 'application/json')
+            .send('name=test')
+            .send('password=password')
+            .expect(201)
+        })
+        
+        it('returns 400 because of duplicate username', () => {
+            expect(true).toBe(true)
+        })
     
-    .then(res => { console.log(res.headers) });
-});
+        it('returns 400 because name is too long', () => {
+            return request(app)
+            .post('/api/authentication/register')
+            .set('Accept', 'application/json')
+            .send('name=123456789_12345') // name length == 15
+            .send('password=password')
+            .expect(400)
+        });
+    
+        it('returns 400 because name is too short', () => {
+            return request(app)
+            .post('/api/authentication/register')
+            .set('Accept', 'application/json')
+            .send('name=')
+            .send('password=password')
+            .expect(400)
+        });
+        
+        it('returns 400 because password is too long', () => {
+            return request(app)
+            .post('/api/authentication/register')
+            .set('Accept', 'application/json')
+            .send('name=name')
+            .send('password=_123456789_123456789_123456789_123456789_123456789') // password length == 50
+            .expect(400)
+        });
+    
+        it('returns 400 because name is too short', () => {
+            return request(app)
+            .post('/api/authentication/register')
+            .set('Accept', 'application/json')
+            .send('name=')
+            .send('password=password')
+            .expect(400)
+        });
+    });
+
+    describe('POST /api/login',  () => {
+        
+    })
+})
+
