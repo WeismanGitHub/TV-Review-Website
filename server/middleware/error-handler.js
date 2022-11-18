189b7d74f5d352782b3a57d1661bcbb7d0b7c2eb
Weisman <weisman123@outlook.com>
2022-11-18 04:54:20 -0800

    made printing errors conditional

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,16 +1,17 @@
 const errorHandler = (err, req, res, next) => {
+    if (process.env.PRINT_ERRORS == 'true') { // Won't work without the '== true' part
+        console.error(err.message)
+    }
+
     if (err.message == 'Request failed with status code 404') {
-        console.log(err)
         return res.status(404).send('Not found!')
     }
     
     if (err.code == 'ENOTFOUND') {
-        console.log(err.message)
         return res.status(500).send('Error getting results.')
     }
     
-    console.log(err)
-    res.status(err.statusCode || 500).send(String(err.message))
+    res.status(err.statusCode || 500).send(err.message)
 }
 
 module.exports = errorHandler
