7520fe5d767756a9f9854b53425085ecc4231f10
Weisman <weisman123@outlook.com>
2022-11-18 00:32:40 -0800

    destructure BadRequestError

--- server/controllers/authentication-controllers.js
+++ server/controllers/authentication-controllers.js
@@ -1,19 +1,19 @@
 const UserModel = require('../models/user-model')
-const CustomErrors = require('../errors')
+const { BadRequestError } = require('../errors')
 
 const register = async (req, res) => {
     const { name, password } = req.body
     
-    if (!name) throw new CustomErrors.BadRequestError('Name is undefined.')
-    if (!password) throw new CustomErrors.BadRequestError('Password is undefined.')
+    if (!name) throw new BadRequestError('Name is undefined.')
+    if (!password) throw new BadRequestError('Password is undefined.')
 
-    if ((name.length >= 15) || (name.length <= 0)) throw new CustomErrors.BadRequestError('Name must be between 0 and 15 characters.')
-    if ((password.length >= 50) || (password.length <= 6)) throw new CustomErrors.BadRequestError('Password must be between 6 and 50 characters.')
+    if ((name.length >= 15) || (name.length <= 0)) throw new BadRequestError('Name must be between 0 and 15 characters.')
+    if ((password.length >= 50) || (password.length <= 6)) throw new BadRequestError('Password must be between 6 and 50 characters.')
 
     const userAlreadyExists = await UserModel.exists({ name: name });
 
     if (userAlreadyExists) {
-        throw new CustomErrors.BadRequestError('Username already exists.')
+        throw new BadRequestError('Username already exists.')
     }
 
     const user = await UserModel.create(req.body)
@@ -30,13 +30,13 @@ const login = async (req, res) => {
     const user = await UserModel.findOne({ name: name })
 
     if (!user) {
-        throw new CustomErrors.BadRequestError('Please provide a valid name.')
+        throw new BadRequestError('Please provide a valid name.')
     }
     
     const passwordIsCorrect = await user.checkPassword(password)
     
     if (!passwordIsCorrect) {
-        throw new CustomErrors.BadRequestError('Please provide the correct password.')
+        throw new BadRequestError('Please provide the correct password.')
     }
 
     const token = user.createJWT()
