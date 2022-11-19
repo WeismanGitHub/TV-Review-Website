a6dd14a7dd7bc85ba4f9890c6c765548a384c384
Weisman <weisman123@outlook.com>
2022-11-18 18:11:29 -0800

    added NotFoundError

--- server/errors/index.js
+++ server/errors/index.js
@@ -1,11 +1,13 @@
 const UnauthenticatedError = require('./unauthenticated');
-const BadRequestError = require('./bad-request');
+const NotFoundError = require('./not-found');
 const UnauthorizedError = require('./unauthorized');
+const BadRequestError = require('./bad-request');
 const CustomError = require('./custom-error');
 
 module.exports = {
     UnauthenticatedError,
     UnauthorizedError,
     BadRequestError,
+    NotFoundError,
     CustomError,
 };
