8bd39da37c92e38574a30843ef28ab4c6dad86c7
Weisman <weisman123@outlook.com>
2022-11-19 02:01:54 -0800

    set up

---
+++ server/tests/media.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('media', () => {
+    describe('POST /api/media/search',  () => {
+    });
+
+    describe('POST /api/media/trending',  () => {
+    })
+
+    describe('POST /api/media/:type/:id',  () => {
+    })
+})
+
