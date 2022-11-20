ce0dcb15768bf45969046a2b74459410d02c16c8
Weisman <weisman123@outlook.com>
2022-11-20 00:29:01 -0800

    created new test

--- server/tests/media.test.js
+++ server/tests/media.test.js
@@ -3,6 +3,19 @@ const app = require('../app');
 
 describe('media', () => {
     describe('POST /api/media/search',  () => {
+        it('returns 200 because query is valid', () => {
+            return request(app)
+            .get('/api/media/search')
+            .query({ phrase: 'test' })
+            .expect(200)
+            .then(res => {
+                res._body.forEach(media => {
+                    expect(media).toHaveProperty('title')
+                    expect(media).toHaveProperty('id')
+                    expect(media).toHaveProperty('mediaType')
+                })
+            })
+        })
     });
 
     describe('POST /api/media/trending',  () => {
