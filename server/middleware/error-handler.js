9f617420ee8d284a4010cd09247fc648326c2a4b
Weisman <weisman123@outlook.com>
2022-11-21 23:37:45 -0800

    removed extra import

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,10 +1,8 @@
-const { InternalError } = require('../errors')
-
 const errorHandler = (err, req, res, next) => {
     if (process.env.PRINT_ERRORS == 'true') { // Won't work without the '== true' part
         console.error(err.message)
     }
-    
+
     res.status(err.statusCode || 500).send(err.message)
 }
 
