99f7d385c42b7c15d54ad318de4828dc618e060a
Weisman <weisman123@outlook.com>
2022-11-21 23:35:43 -0800

    updated erro handler

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,14 +1,8 @@
+const { InternalError } = require('../errors')
+
 const errorHandler = (err, req, res, next) => {
     if (process.env.PRINT_ERRORS == 'true') { // Won't work without the '== true' part
         console.error(err.message)
-    }
-
-    if (err.message == 'Request failed with status code 404') {
-        return res.status(404).send('Not found!')
-    }
-    
-    if (err.code == 'ENOTFOUND') {
-        return res.status(500).send('Error getting results.')
     }
     
     res.status(err.statusCode || 500).send(err.message)
