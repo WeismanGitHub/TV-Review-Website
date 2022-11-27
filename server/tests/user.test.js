3d20a5cde068c48dbe3f72e31bc4e7b927cbb280
Weisman <weisman123@outlook.com>
2022-11-27 00:24:59 -0800

    created new test

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -1,7 +1,19 @@
+const UserModel = require('../models/user-model')
 const request = require('supertest');
 const app = require('../app');
 
 describe('user tests', () => {
+    let token;
+    
+    beforeAll(async () => {
+        const user = await UserModel.create({ name: 'test', password: 'password' })
+        token = user.createJWT()
+    })
+
+    afterAll(async () => {
+        const user = await UserModel.deleteMany({ name: 'test' })
+    })
+
     describe('GET /api/user',  () => {
         it('returns 400 because id is invalid', () => {
             return request(app)
@@ -15,5 +27,17 @@ describe('user tests', () => {
             .expect(404)
         })
     });
+
+    describe('PATCH /api/user',  () => {
+        it('returns 200 because request is valid', () => {
+            return request(app)
+            .patch('/api/user/')
+            .set('Cookie', [`token=${token}`,])
+            .send('currentPassword=password')
+            .send('newName=newName')
+            .send('newPassword=newPassword')
+            .expect(200)
+        })
+    })
 })
 
